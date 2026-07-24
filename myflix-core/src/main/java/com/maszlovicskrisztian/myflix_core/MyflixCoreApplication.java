33f1a7d468fca62167cf35262381b30569e1e154
maszlovicskrisztian <maszlovics.krisztian@gmail.com>
2026-07-24 19:49:58 +0200

    Project init

---
+++ myflix-core/src/main/java/com/maszlovicskrisztian/myflix_core/MyflixCoreApplication.java
@@ -0,0 +1,13 @@
+package com.maszlovicskrisztian.myflix_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyflixCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyflixCoreApplication.class, args);
+	}
+
+}
