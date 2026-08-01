f2b2d2f90c53a9900c282ca1661785e6858e4272
maszlovicskrisztian <maszlovics.krisztian@gmail.com>
2026-08-01 16:51:57 +0200

    FE: Metadata include

--- myflix-frontend/src/app/model/media-item.ts
+++ myflix-frontend/src/app/model/media-item.ts
@@ -1,6 +1,13 @@
 export type MediaItem = {
     id: number;
     fileName: string;
-    durationSeconds: number | null;
     addedAt: string;
+
+    tmdbId: number | null;
+    overview: string | null;
+    title: string | null;
+    posterPath: string | null;
+    backdropPath: string | null;
+    releaseDate: string | null;
+    runtimeMinutes: number | null;
 }
